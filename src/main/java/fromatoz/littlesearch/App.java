da4f07783cfdf7babd1aea0e083636b3fb5ebfca
mariliercyril <cyril.marilier@etu.unice.fr>
2018-11-18 13:34:42 +0100

    Initialize the project: create a Maven Standard Directory Layout (#1)

---
+++ src/main/java/fromatoz/littlesearch/App.java
@@ -0,0 +1,13 @@
+package fromatoz.littlesearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

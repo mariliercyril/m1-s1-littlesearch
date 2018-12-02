55d0a527a4b1a6b7e8b8409df9e08a30e7e716d9
mariliercyril <cyril.marilier@etu.unice.fr>
2018-12-02 14:52:19 +0100

    Perfect the Javadoc of the class 'DataIntegrator'

--- src/main/java/edu/fromatoz/littlesearch/app/DataIntegrator.java
+++ src/main/java/edu/fromatoz/littlesearch/app/DataIntegrator.java
@@ -23,26 +23,59 @@ import edu.fromatoz.littlesearch.dataintegrator.model.JSONWriter;
 import edu.fromatoz.littlesearch.tool.Separator;
 
 /**
- * The {@code DataIntegrator} class is the "main" class of a data integrator for the search engine.
- * <p>This is a sort of ETL (Extract-Transform-Load) application: it extracts data from various pages of the CNRTL,
+ * The {@code DataIntegrator} class is the "main" class of an integrator of data for the search engine.
+ * <p>This is a sort of ETL (Extract-Transform-Load) application: this extracts data from several pages of
+ * the <a href="http://www.cnrtl.fr">CNRTL</a> (Centre National de Ressources Textuelles et Lexicales),
  * transforms it by performing NLP (Natural Language Processing) tasks (among others, <i>tokenization</i>
  * with a {@link FrenchAnalyser}, <i>part-of-speech tagging</i> with a {@link FrenchTagger} and <i>lemmatisation</i>
- * with a {@link FrenchLemmatiser}) and loads it into a data warehouse.</p>
- * <p>This integrator extracts data from the <a href="http://www.cnrtl.fr">CNRTL</a> (Centre National de Ressources Textuelles et Lexicales).</p>
+ * with a {@link FrenchLemmatiser}) and loads it into a warehouse.</p>
  * 
  * @author Andrei Zabolotnîi
  * @author Cyril Marilier
  */
 public class DataIntegrator {
 
+	/**
+	 * The <b>communication protocol</b>.
+	 */
+	private static final String COMMUNICATION_PROTOCOL = "http";
+	/**
+	 * Format of the path of a resource.
+	 */
 	private static final String RESOURCE_PATH_FORMAT = Separator.SLASH.getValue() + "%s";
 
-	private static final String CNRTL_URL_FORMAT = "http" + Separator.COLON.getValue() + "//" + "www.cnrtl.fr" + RESOURCE_PATH_FORMAT;
+	/**
+	 * The CNRTL's <b>domain name</b>.
+	 */
+	private static final String CNRTL_DOMAIN_NAME = "www.cnrtl.fr";
+	/**
+	 * The URL of the CNRTL's home page.
+	 */
+	private static final String CNRTL_HOME_PAGE_URL = COMMUNICATION_PROTOCOL + Separator.COLON.getValue() + "//" + CNRTL_DOMAIN_NAME;
+	/**
+	 * Format of the URL of a CNRTL's page.
+	 */
+	private static final String CNRTL_URL_FORMAT = CNRTL_HOME_PAGE_URL + RESOURCE_PATH_FORMAT;
 
+	/**
+	 * Format of the URL of the CNRTL's page of definition of a word.
+	 */
 	public static final String DEFINITION_FORMAT = String.format(CNRTL_URL_FORMAT, "definition") + RESOURCE_PATH_FORMAT;
+	/**
+	 * Format of the URL of the CNRTL's page of morphology of a word.
+	 */
 	public static final String MORPHOLOGY_FORMAT = String.format(CNRTL_URL_FORMAT, "morphologie") + RESOURCE_PATH_FORMAT;
+	/**
+	 * Format of the URL of the CNRTL's page of synonymy of a word.
+	 */
 	public static final String SYNONYMY_FORMAT = String.format(CNRTL_URL_FORMAT, "synonymie") + RESOURCE_PATH_FORMAT;
 
+	/**
+	 * Allows a developer to integrate data from the text of the corpus.
+	 * 
+	 * @param args
+	 *  the name of the file of the text which we would to integrate
+	 */
 	public static void main(String[] args) {
 
 		// Gives the text file name to the integrator...
@@ -66,13 +99,13 @@ public class DataIntegrator {
 			while ((paragraph = bufferedReader.readLine()) != null) {
 				// Tokenizes the current paragraph...
 				FrenchAnalyser frenchAnalyser = new FrenchAnalyser(paragraph);
-				// Gets the token after analysing the current paragraph...
-				Set<String> terms = frenchAnalyser.getToken();
-				for (String term : terms) {
+				// Gets the token after analysing the current paragraph
+				Set<String> words = frenchAnalyser.getToken();
+				for (String word : words) {
 					// For monitoring...
-					System.out.println(term);
-					// Writes the term data by integrating it...
-					jsonWriter.write(term);
+					System.out.println(word);
+					// Loads the word data into the warehouse
+					jsonWriter.load(word);
 				}
 			}
 		} catch (FileNotFoundException fnfe) {
@@ -92,22 +125,54 @@ public class DataIntegrator {
 
 	/**
 	 * The {@code CNRTLParser} class defines a parser for the HTML pages of the CNRTL.
+	 * <p>Several of the tools which we have developed for the data integrator
+	 * need to parse the HTML of CNRTL's pages.</p>
 	 * 
 	 * @author Andrei Zabolotnîi
 	 * @author Cyril Marilier
 	 */
 	public static class CNRTLParser {
 
+		/**
+		 * Returns the first of the elements returned by the value of the CSS class (or the CSS ID).
+		 * 
+		 * @param url
+		 *  the URL of the CNRTL's page in question
+		 * @param cssQuery
+		 *  the CSS containing the value of the class (or the id)
+		 * 
+		 * @return elements by the value of the CSS class (or the CSS ID).
+		 */
 		public Element getFirstHTMLElement(String url, String cssQuery) {
 
 			return getHTMLElements(url, cssQuery).first();
 		}
 
+		/**
+		 * Returns the last of the elements returned by the value of the CSS class (or the CSS ID).
+		 * 
+		 * @param url
+		 *  the URL of the CNRTL's page in question
+		 * @param cssQuery
+		 *  the CSS containing the value of the class (or the id)
+		 * 
+		 * @return elements by the value of the CSS class (or the CSS ID).
+		 */
 		public Element getLastHTMLElement(String url, String cssQuery) {
 
 			return getHTMLElements(url, cssQuery).last();
 		}
 
+		/**
+		 * Returns elements by the value of the value of the CSS class (or the CSS ID).
+		 * 
+		 * @param url
+		 *  the URL of the CNRTL's page in question
+		 * @param cssQuery
+		 *  the CSS containing the value of the class (or the id)
+		 * 
+		 * @return elements by the value of the CSS class (or the CSS ID).
+		 */
 		public Elements getHTMLElements(String url, String cssQuery) {
 
 			try {

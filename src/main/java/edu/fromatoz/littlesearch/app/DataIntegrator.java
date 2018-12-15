0b9357bee6583a9389a0fe6cafb8d4c5e176a9ad
mariliercyril <cyril.marilier@etu.unice.fr>
2018-12-15 17:45:05 +0100

    Add some new JSON files

--- src/main/java/edu/fromatoz/littlesearch/app/DataIntegrator.java
+++ src/main/java/edu/fromatoz/littlesearch/app/DataIntegrator.java
@@ -80,7 +80,7 @@ public class DataIntegrator {
 	public static void main(String[] args) {
 
 		// Gives the text file name to the integrator...
-		String textFileName = "CAMILLE_JORDAN";
+		String textFileName = "EUCLIDE";
 		if (args.length > 0) {
 			textFileName = args[0];
 		}

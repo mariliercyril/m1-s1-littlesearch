1708b704ba7c777f7a133bc8b9b23c15f0d01ba1
mariliercyril <cyril.marilier@etu.unice.fr>
2018-12-25 20:18:48 +0100

    Remove an useless import

--- src/main/java/edu/fromatoz/littlesearch/dataintegrator/FrenchAnalyser.java
+++ src/main/java/edu/fromatoz/littlesearch/dataintegrator/FrenchAnalyser.java
@@ -8,7 +8,6 @@ import java.util.TreeSet;
 
 import edu.fromatoz.littlesearch.dataintegrator.model.JSONWriter;
 
-import edu.fromatoz.littlesearch.tool.Separator;
 import edu.fromatoz.littlesearch.tool.ValuesFileReader;
 
 import org.apache.log4j.Logger;
